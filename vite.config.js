d971265bb80ca8374218ac5620c184545294608f
dhsig86 <dr.dhsig@gmail.com>
2026-03-25 21:06:49 -0300

    feat: integra modo Voz Neural com Transformers.js MMS-TTS e processamento emocional via Web Audio API

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/OTTO-VOICE/',
+  optimizeDeps: {
+    exclude: ['@huggingface/transformers'],
+  },
   test: {
     environment: 'jsdom',
     globals: true,
